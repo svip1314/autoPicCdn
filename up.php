c48002b41cd45ab6f48ac50e8c87267fb2e3f448
svip1314 <77183153+svip1314@users.noreply.github.com>
2021-02-11 19:34:57 +0800

    Update up.php

--- up.php
+++ up.php
@@ -30,27 +30,27 @@ if(!is_callable('curl_init')){
 }
 
 //必选项
-define("TYPE","GITHUB");//选择github
+define("TYPE","github");//选择github
 //define("TYPE","GITEE");//选择gitee，如果使用gitee，需要手动建立master分支，可以看这里 https://gitee.com/help/articles/4122
 
-define("USER","pic-cdn");//你的GitHub/Gitee的用户名
+define("USER","svip1314");//你的GitHub/Gitee的用户名
 
-define("REPO","cdn2");//必须是上面用户名下的 公开仓库
+define("REPO","autoPicCdn");//必须是上面用户名下的 公开仓库
 
 define("MAIL","yumusb@foxmail.com");//邮箱无所谓，随便写
 
-define("TOKEN","213");
+define("TOKEN","468368001550067c88c30279cec9febc4f8fb518");
 // Github 去这个页面 https://github.com/settings/tokens生成一个有写权限的token（repo：Full control of private repositories 和write:packages前打勾）
 // gitee  去往这个页面 https://gitee.com/personal_access_tokens
 
 //数据库配置文件
 //请确保把当前目录下的 pic.sql 导入到你的数据库
 $database = array(
-        'dbname' => 'YourDbName',//你的数据库名字
+        'dbname' => 'gittc',//你的数据库名字
         'host' => 'localhost',
         'port' => 3306,
-        'user' => 'YourDbUser',//你的数据库用户名
-        'pass' => 'YourDbPass',//你的数据库用户名对应的密码
+        'user' => 'gittc',//你的数据库用户名
+        'pass' => 'zyy123456789',//你的数据库用户名对应的密码
     );
     
 
